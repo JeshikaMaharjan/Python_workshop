152f4d0de53a0b24b5797f697bfaab70a366aeef
Jeshika Maharjan <jeshika99@gmail.com>
2022-07-12 17:28:08 +0545

    Basics in python

---
+++ 1_Intro.py
@@ -0,0 +1,34 @@
+#Basics
+
+"Hello" * 3  #implies Hello 3 times print hunxa
+#// implies quotient matra auxa
+# ** implies square (3**3 = 27)
+
+#--------------------------------------
+
+name = input("String to be print: ")
+print(name)
+
+#--------------------------------------
+
+address = input( " Enter your address: ")
+print ("Your adrress is", address)
+
+address = input( " Enter your address: ")
+print(f"Your adrress is {address}") #replaces address with it's value 
+
+#--------------------------------------
+
+print("New line \n new line") #"" yo vitra ko text is raw string
+print(r"New line \n new line") #r le ignores raw string
+
+#--------------------------------------
+
+dob = input("Enter birth year") #esma input le string input linxa so first convert to int
+age = 2022 - int(dob)
+print ("Your age:", age)
+
+#--------------------------------------
+
+
+
