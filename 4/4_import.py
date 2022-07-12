f3ffcb8ec24f6ee8549a22c78008d16d7b2a586c
Jeshika Maharjan <jeshika99@gmail.com>
2022-07-12 17:34:34 +0545

    Importing functions

---
+++ 4/4_import.py
@@ -0,0 +1,9 @@
+# import calc #import garne filename don't start with number
+# print(calc.add(4,5))
+
+from calc import add #here add rakhyo vane calc ma vakoo add matra import hunxa
+from calc import add,sub #here  add,sub  import hunxa
+from calc import * #here * rakhyo vane calc ma vakoo savaai import hunxa
+
+print(add(4,5))
+print(sub(4,5))
